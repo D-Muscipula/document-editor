bc34b720aead330853facfdd90b515ff7ef6753d
Sergei Kozhnev <s4-7@yandex.ru>
2024-11-25 14:17:42 +0300

    Initial commit

---
+++ src/main/kotlin/com/example/document_editor/DocumentEditorApplication.kt
@@ -0,0 +1,11 @@
+package com.example.document_editor
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DocumentEditorApplication
+
+fun main(args: Array<String>) {
+	runApplication<DocumentEditorApplication>(*args)
+}
