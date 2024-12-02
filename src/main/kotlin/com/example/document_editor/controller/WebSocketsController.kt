2953609876dacf8e04999b4630b4b75cde3abcd5
Sergei Kozhnev <s4-7@yandex.ru>
2024-12-02 12:23:58 +0300

    refactoring

--- src/main/kotlin/com/example/document_editor/controller/WebSocketsController.kt
+++ src/main/kotlin/com/example/document_editor/controller/WebSocketsController.kt
@@ -1,7 +1,6 @@
 package com.example.document_editor.controller
 
 import com.example.document_editor.dto.Message
-import com.example.document_editor.dto.Messages
 import com.example.document_editor.dto.OutputMessage
 import com.example.document_editor.model.Document
 import com.example.document_editor.service.DocumentService
@@ -11,13 +10,10 @@ import org.springframework.messaging.handler.annotation.DestinationVariable
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.messaging.handler.annotation.SendTo
-import org.springframework.stereotype.Controller
-import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
-import java.util.*
-import com.fasterxml.jackson.module.kotlin.readValue
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 
 @RestController
@@ -40,40 +36,14 @@ class WebSocketsController(private val documentService: DocumentService) {
         val jsonString: String = objectMapper.writeValueAsString(message)
         val documentFromDb = documentService.findByIdAndTitle(UUID.fromString(uuid), title)
 
-        val document = Document(id = UUID.fromString(uuid),
-                title = title,
-                contentDelta = jsonString)
-            documentService.saveDocument(document)
+        val document = Document(
+            id = UUID.fromString(uuid),
+            title = title,
+            contentDelta = jsonString
+        )
+        documentService.saveDocument(document)
 
-//        if (documentFromDb?.contentDelta == null) {
-//            val messages = Messages()
-//            messages.messages.add(message)
-//            val jsonStr: String = objectMapper.writeValueAsString(messages)
-//            val document = Document(id = UUID.fromString(uuid),
-//                title = title,
-//                contentDelta = jsonStr)
-//            documentService.saveDocument(document)
-//        } else {
-//            val messages: Messages = objectMapper.readValue(documentFromDb.contentDelta!!)
-//            messages.messages.add(message)
-//            val jsonStr: String = objectMapper.writeValueAsString(messages)
-//            val document = Document(
-//                id = UUID.fromString(uuid),
-//                title = title,
-//                contentDelta = jsonStr
-//            )
-//            documentService.saveDocument(document)
-//        }
         return OutputMessage(message.delta)
-    }
-
-    @GetMapping("/documents")
-    fun getDocumentsPage(model: Model): String {
-        val myUUID = UUID.randomUUID().toString()
-        val myName = "John Doe"
-        model.addAttribute("uuid", myUUID)
-        model.addAttribute("name", myName)
-        return "document.html"
     }
 
     @GetMapping("/document/{title}/{id}")
